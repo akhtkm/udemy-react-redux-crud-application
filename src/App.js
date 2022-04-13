0a13a218dff05909881ff81221051dedd110dcd9
akhtkm <akihito.kimuar.15@gmail.com>
2022-04-14 00:43:35 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -10,9 +10,25 @@ import React, { Component } from 'react';
 //   }
 // }
 
+// class App extends Component {
+//   render() {
+//     const greeting = "Hi, Tom!"
+//     const dom = <h1 className="foo">{greeting}</h1>
+//     return dom;
+//   }
+// }
+
 class App extends Component {
   render() {
-    return <h1>Hello, world!!</h1>;
+    // return <input type="text" onClick={() => { console.log("I am clicked") }} />;
+    return (
+      // <div>
+      <React.Fragment>
+        <label htmlFor='bar'>bar</label>
+        <input type="text" onChange={() => { console.log("I am clicked") }} />
+      </React.Fragment>
+      // </div>
+    )
   }
 }
 
