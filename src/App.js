85184c69e8eea58fdfff9c416cac86732d1578bb
akhtkm <akihito.kimuar.15@gmail.com>
2022-04-14 00:29:23 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, wolrd!!"
+//     )
+//   }
+// }
+
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    return <h1>Hello, world!!</h1>;
   }
 }
 
